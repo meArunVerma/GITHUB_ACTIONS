9b868203b1a2fd6813c8698d92947d6d5de7a9ae
arunv0710@gmail.com <arunv0710@gmail.com>
2026-04-30 21:14:45 +0530

    Initial commit: Added backend, frontend and Github Actions yaml files

---
+++ FRONTEND/myapp/src/App.jsx
@@ -0,0 +1,11 @@
+const App = ()=>{
+  return (
+    <>
+       <h1 className="text-2xl text-red-500 font-bold">
+          If this is red, Tailwind works
+      </h1>
+    </>
+  )
+}
+
+export default App;
